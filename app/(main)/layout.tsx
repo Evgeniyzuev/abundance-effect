866d41d8581562ab5e984de99f35188e8060e103
Evgeniy <evgeniyzuev43@gmail.com>
2025-11-25 23:41:21 +0300

    p-3

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,7 +7,7 @@ export default function MainLayout({
 }) {
     return (
         <div className="flex min-h-screen flex-col bg-ios-background pb-14">
-            <main className="flex-1 p-4">
+            <main className="flex-1 p-3">
                 {children}
             </main>
             <BottomNav />
