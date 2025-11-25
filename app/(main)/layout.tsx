106e77d67e1550669cf02d5b2286c17390dea088
Evgeniy <evgeniyzuev43@gmail.com>
2025-11-25 23:34:46 +0300

    main layout pb-14

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex min-h-screen flex-col bg-ios-background pb-16">
+        <div className="flex min-h-screen flex-col bg-ios-background pb-14">
             <main className="flex-1 p-4">
                 {children}
             </main>
